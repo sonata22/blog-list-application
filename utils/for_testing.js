bbdde7d93c678d2d7a163fdff5bd3e9beec20bbd
sonata22 <nataliia.sosnovshchenko@gmail.com>
2024-02-05 14:40:48 +0200

    4.5 done

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -21,8 +21,20 @@ const totalLikes = (blogs) => {
         : blogs.reduce((acc, value) => acc + value.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length) {
+        let maxValue = 0
+        blogs.map((blog) => {
+            maxValue = Math.max(maxValue, blog.likes)
+        })
+        return blogs.find((blog) => blog.likes === maxValue)
+    }
+    return {}
+}
+
 module.exports = {
     reverse,
     average,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
